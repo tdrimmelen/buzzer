7845c91588beb6f506c667f779919601a8fdc79f
Theo van Drimmelen <theo@vandrimmelen.eu>
2020-10-11 16:54:14 +0200

    fix

--- __main__.py
+++ __main__.py
@@ -1,6 +1,11 @@
 from clockbuzzer import Clockbuzzer
+import logging
+import os
 
 if __name__ == "__main__":
+
+    LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+    logging.basicConfig(level=LOGLEVEL)
 
     clockbuzzer = Clockbuzzer()
 
