83caa454714fd00de1189eebbdb4adb2c706167a
Theo van Drimmelen <theo@vandrimmelen.eu>
2024-09-01 16:39:53 +0200

    Fix

--- install/prepare.sh
+++ install/prepare.sh
@@ -42,7 +42,7 @@ fi
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -55,7 +55,7 @@ echo \
 sudo apt-get update
 
 # install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo usermod -aG docker $USER
 
